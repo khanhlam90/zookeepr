87ed87369903f8ab0e66318e8718a143dae28c1f
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2021-12-18 19:17:13 -0800

    functioning API handling GET and POST requests

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const express = require('express');
 const { animals } = require('./data/animals.json');
 
-const express = require('express');
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+// because require() of animals only create a copy of animals to use in this server.js
+// push method does not override the orignal file, therefore, have to import and use the fs library to write that data to animals.json
+const fs = require('fs');
+const path = require('path'); //module of node.js
+
+// parse incoming string or array data -  middleware functions need to be set up every time you create a server that's looking to accept POST data.
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data - POST
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -50,6 +58,37 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  //console.log(body);
+
+  // our function's main code will go here!
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  // return finished code to post route for response
+  return body;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -66,6 +105,27 @@ app.get('/api/animals/:id', (req, res) => {
       res.send(404);
     }
   });
+
+// allow user to add data 11.2 - start
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+  //console.log(req.body);
+
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+  // add animal to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
+
+  res.json(animal);
+  }
+});
+
+// allow user to add data 11.2 - end
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
